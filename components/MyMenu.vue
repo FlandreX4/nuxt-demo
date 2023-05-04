cb0bc31d1f7de9f5cee513e985191cdebe5a08c9
FlandreX4 <68147219+FlandreX4@users.noreply.github.com>
2023-05-04 15:00:40 +0800

    init
    init

---
+++ components/MyMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="width: 300px">
+    <a-menu
+      mode="vertical"
+    >
+      <template v-for="item in list">
+        <a-menu-item v-if="!item.children" :key="item.key">
+          <a-icon type="pie-chart" />
+          <span>{{ item.title }}</span>
+        </a-menu-item>
+        <MySubMenu v-else :key="item.key" :menu-info="item" />
+      </template>
+    </a-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  name: "",
+  data() {
+    return {
+    };
+  },
+  components: {},
+  mounted() {},
+  methods: {
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <MyMenu :list="list" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      list: [
+        {
+          key: "1",
+          title: "Option 1",
+        },
+        {
+          key: "2",
+          title: "Navigation 2",
+          children: [
+            {
+              key: "2.1",
+              title: "Navigation 3",
+              children: [{ key: "2.1.1", title: "Option 2.1.1" }],
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
